97daa89266d8cb652baed04fce4f879c3af6a5b1
Michael Thomas <mthomas@ketchbrookanalytics.com>
2026-04-08 15:25:20 -0400

    DRY

--- R/aaa.R
+++ R/aaa.R
@@ -12,22 +12,22 @@ ffiec_error_message <- function(resp) {
 #' @noRd
 check_empty_creds <- function(user_id, bearer_token) {
 
+  msg <- paste(
+    "is missing. If you do not have an account with the FFIEC's web service,",
+    "you can register for one at",
+    "{.url https://cdr.ffiec.gov/public/PWS/CreateAccount.aspx?PWS=true}."
+  )
+
   if (is.null(user_id) || trimws(user_id) == "") {
-    paste(
-      "`user_id` is missing. If you do not have a UserID with the FFIEC's",
-      "web service, you can register for one at",
-      "{.url https://cdr.ffiec.gov/public/PWS/CreateAccount.aspx?PWS=true}."
-    ) |>
-      cli::cli_abort()
+    cli::cli_abort(
+      paste("{.var user_id}", msg)
+    )
   }
 
   if (is.null(bearer_token) || trimws(bearer_token) == "") {
-    paste(
-      "`bearer_token` is missing. If you do not have a UserID with the FFIEC's",
-      "web service, you can register for one at",
-      "{.url https://cdr.ffiec.gov/public/PWS/CreateAccount.aspx?PWS=true}."
-    ) |>
-      cli::cli_abort()
+    cli::cli_abort(
+      paste("{.var bearer_token}", msg)
+    )
   }
 
 }
